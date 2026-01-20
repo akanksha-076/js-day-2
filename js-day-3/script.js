ac632bd689ab69a0bebcb07e4d0c4a1f47d5849e
Akanksha <a31695132@gmail.com>
2026-01-20 15:16:32 +0530

    javascript day 3

---
+++ js-day-3/script.js
@@ -0,0 +1,15 @@
+//let fruit1=["apple","mango"]
+//let fruit2=["banana","cherry"]
+//console.log([...fruit1,...fruit2])
+
+//let age=18;
+//lat canvote =age>=18?"you can vote":"you cant vote";
+//console.log(canvote);
+
+//let num=5
+//let result= num%2===0?"num is odd":"num is even";
+//console.log(result)
+
+let year=2026
+let result=(year%4===0 & year%100!==0 || year%400===0)?"year is a leap year":"year is not a lear year";
+console.log(result)
