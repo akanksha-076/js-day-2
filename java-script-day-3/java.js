b165031f655b66c439dc244fb8644fe2c09e50ab
Akanksha <a31695132@gmail.com>
2026-01-21 14:17:08 +0530

    string methods

---
+++ java-script-day-3/java.js
@@ -0,0 +1,20 @@
+let str = "JavaScript is fun!";
+let sliced = str.slice(0, 10);
+console.log(sliced)
+
+let string = "JavaScript is amazing!";
+let sub = string.substring(11, 17);
+console.log(sub)
+
+let sentence = "I,am,a,comma,separated,sentence";
+let words = sentence.split(",");
+console.log(words)
+
+// find out the occurence of r in thr given string
+let strr= "tu meri mai tera mai tera tu meri"
+let w = strr.split("r");
+console.log(w.length-1)
+
+
+
+
