6aac917bfbb98f2da1d834a24ad121ae48273e66
Akanksha <a31695132@gmail.com>
2026-02-09 15:41:30 +0530

    js day 09-2

--- js-day-09/script.js
+++ js-day-09/script.js
@@ -8,7 +8,17 @@ function handleclicks(){
     document.getElementById("head").style.color="tomato"
 
 }
-function handleClick(){
+// function handleClick(){
+//     const spanEle=document.createElement("span").textContent="hello im span element"
+//     const image=document.createElement("img");
+//     image.setAttribute("src","https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
+//     image.setAttribute("alt","K.R.Mangalam")
+//     const gallery=document.getElementById("content")
+//     gallery.append(image)
+//     gallery.append(spanEle)
+
+// }
+document.getElementById("btn").addEventListener("click",()=>{
     const spanEle=document.createElement("span").textContent="hello im span element"
     const image=document.createElement("img");
     image.setAttribute("src","https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
@@ -16,6 +26,5 @@ function handleClick(){
     const gallery=document.getElementById("content")
     gallery.append(image)
     gallery.append(spanEle)
-
-}
+})
 
