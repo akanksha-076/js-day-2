b20d509597a4fd1da63af59af873b3daa2009a9d
Akanksha <a31695132@gmail.com>
2026-02-25 13:46:18 +0530

    js day 15

--- js-day-14/script.js
+++ js-day-14/script.js
@@ -1,21 +1,20 @@
-// console.log("frist line")
-// setTimeout(()=>{
-//     console.log("interval")
-// },0)
-// const p=new Promise((resolve,reject)=>{
-//     resolve()
-// })
-// p.then(()=>{
-//     console.log("inside promise")
-// })
-// const p2=new Promise((resolve,reject)=>{
-//     resolve()
-// })
-// p2.then(()=>{
-//     console.log("inside promise")
-// })
+console.log("frist line")
+setTimeout(()=>{
+    console.log("interval")
+},0)
+const p=new Promise((resolve,reject)=>{
+    resolve()
+})
+p.then(()=>{
+    console.log("inside promise")
+})
+const p2=new Promise((resolve,reject)=>{
+    resolve()
+})
+p2.then(()=>{
+    console.log("inside promise")
+})
     
-
-// console.log("last line")
+console.log("last line")
 
 
